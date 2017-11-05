e9506c4604f02d1394215efe3ea8f349d650e593
qafrankly <31970630+qafrankly@users.noreply.github.com>
2017-11-05 15:04:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ import { bylineToString, getLocalLink } from 'utils/NewsUtils';
 import { COLOR_STYLE } from 'constants/FRNBaseStyles';
 import Video from 'components/media/Video';
 import AdvertisementUnit from 'components/AdvertisementUnit';
+import { Button } from 'antd';
+ReactDOM.render(<Button>xxxx</Button>);
+ 
+      ↓ ↓ ↓ ↓ ↓ ↓
+      
+var _button = require('antd/lib/button');
+ReactDOM.render(<_button>xxxx</_button>);
 class RandomTest extends Component {
   static PropTypes = {
     h1: PropTypes.string,
