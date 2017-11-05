a4db93b97c319bc2a2328b1081bbfb401800582b
qafrankly <31970630+qafrankly@users.noreply.github.com>
2017-11-05 15:28:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ import { COLOR_STYLE } from 'constants/FRNBaseStyles';
 import Video from 'components/media/Video';
 import AdvertisementUnit from 'components/AdvertisementUnit';
 import { Button } from 'antd';
-ReactDOM.render(<Button>xxxx</Button>);
-      
-var _button = require('antd/lib/button');
-ReactDOM.render(<_button>xxxx</_button>);
 class RandomTest extends Component {
   static PropTypes = {
     h1: PropTypes.string,
